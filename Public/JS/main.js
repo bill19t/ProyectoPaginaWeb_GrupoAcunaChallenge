ba2b01ee9388b727a583ebdecd568bd19510fb36
bill19t <bterrelpe@ucvvirtual.edu.pe>
2024-11-21 21:31:57 -0500

    Delete Public directory

--- Public/JS/main.js
+++
@@ -1,4 +0,0 @@
-// Aquí puedes agregar cualquier JavaScript necesario para tu sitio
-document.addEventListener("DOMContentLoaded", function () {
-  // Código para ejecutar cuando el DOM esté cargado
-});
